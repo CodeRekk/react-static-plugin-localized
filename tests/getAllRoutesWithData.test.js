159787f14e06642094319ac362c249e7a1df4a3b
Richard Wohlgemuth <woffrek@outlook.com>
2019-05-06 10:24:09 +0200

    fix eslint errors in test-scripts

--- tests/getAllRoutesWithData.test.js
+++ tests/getAllRoutesWithData.test.js
@@ -1,38 +1,37 @@
 import _ from 'lodash';
-import { getAllRoutesWithData } from "../src/routeBuilder";
+import { getAllRoutesWithData } from '../src/routeBuilder';
 import Input from './assets/input';
 import Output from './assets/output';
 
 const functionResult = getAllRoutesWithData(Input);
 const expectedResult = Output;
 
+/* eslint no-param-reassign: 0 */
 _.map(functionResult, (result, index) => {
-  test("getAllRoutesWithData getData", () => {
+  test('getAllRoutesWithData getData', () => {
     expect(result.getData()).toEqual(expectedResult[index].getData());
   });
 
-  if(result.children) {
+  if (result.children) {
     _.map(result.children, (child, childIndex) => {
-      test("getAllRoutesWithData children getData", () => {
-        expect(child.getData()).toEqual(expectedResult[index]['children'][childIndex].getData());
+      test('getAllRoutesWithData children getData', () => {
+        expect(child.getData()).toEqual(expectedResult[index].children[childIndex].getData());
       });
 
-      test("getAllRoutesWithData children", () => {
-        delete child['getData'];
-        delete expectedResult[index]['children'][childIndex]['getData'];
-        expect(child).toEqual(expectedResult[index]['children'][childIndex]);
+      test('getAllRoutesWithData children', () => {
+        delete child.getData;
+        delete expectedResult[index].children[childIndex].getData;
+        expect(child).toEqual(expectedResult[index].children[childIndex]);
       });
     });
   }
 
-  test("getAllRoutesWithData", () => {
-    delete result['getData'];
-    delete result['children'];
-    delete expectedResult[index]['getData'];
-    delete expectedResult[index]['children'];
+  test('getAllRoutesWithData', () => {
+    delete result.getData;
+    delete result.children;
+    delete expectedResult[index].getData;
+    delete expectedResult[index].children;
     expect(result).toEqual(expectedResult[index]);
   });
 });
-
-
-
+/* eslint no-param-reassign: 1 */
--- tests/getCustomData.test.js
+++ tests/getCustomData.test.js
@@ -1,20 +1,20 @@
-import { getCustomData } from "../src/routeBuilder";
-import customDataDE from "../example/data/custom/de";
-import customDataEN from "../example/data/custom/en";
+import { getCustomData } from '../src/routeBuilder';
+import customDataDE from '../example/data/custom/de';
+import customDataEN from '../example/data/custom/en';
 
 const saveCustomConfig = {
-	propKey: "custom",
-	dataPath: "example/data/custom"
+  propKey: 'custom',
+  dataPath: 'example/data/custom',
 };
 
-test("getCustomDataDE", () => {
-	expect(getCustomData(saveCustomConfig, {id: "de"})).toEqual(
-    {custom: customDataDE}
-	);
+test('getCustomDataDE', () => {
+  expect(getCustomData(saveCustomConfig, { id: 'de' })).toEqual(
+    { custom: customDataDE },
+  );
 });
 
-test("getCustomDataEN", () => {
-  expect(getCustomData(saveCustomConfig, {id: "en"})).toEqual(
-    {custom: customDataEN}
+test('getCustomDataEN', () => {
+  expect(getCustomData(saveCustomConfig, { id: 'en' })).toEqual(
+    { custom: customDataEN },
   );
 });
